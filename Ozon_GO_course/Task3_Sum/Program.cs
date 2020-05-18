3897260fd6c2b07ed7b526a0248016b8c668080c
GrigoriyVoronin <grigoriy.1998@mail.ru>
2020-05-18 16:48:11 +0500

    solved 3 task

--- Ozon_GO_course/Task3_Sum/Program.cs
+++ Ozon_GO_course/Task3_Sum/Program.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 
 namespace Task3_Sum
@@ -10,39 +8,62 @@ namespace Task3_Sum
     {
         private static void Main()
         {
-            var numbers = ReadFile1();
-            var t = TakeTarget();
-            var start = 1;
-            var end = numbers.Count - 2;
-            if (end < 1)
+            var result = 0;
+            var t = true;
+            var diffs = new HashSet<int>();
+            int dif;
+            var simbols = new char[16];
+            var len = 0;
+            int numb;
+            using (var a = new StreamReader("input.txt"))
             {
-                End(0);
-                return;
-            }
-
-            numbers.Sort(1, numbers.Count - 2, null);
-            int currentDiffrent;
-            int oldStart;
-            while (true)
-            {
-                oldStart = start;
-                currentDiffrent = t.Item1 - numbers[start];
-                while (numbers[end] > currentDiffrent && end > start)
-                    end--;
-                if (end <= start)
-                    break;
-
-                currentDiffrent = t.Item1 - numbers[end];
-                while (numbers[start] < currentDiffrent && start < end)
-                    start++;
-                if (oldStart == start)
+                while (!a.EndOfStream)
                 {
-                    End(1);
-                    return;
+                    a.Read(simbols, len, 1);
+                    if (simbols[len] == 10 || simbols[len] == 13 || simbols[len] == 32)
+                    {
+                        if (t)
+                        {
+                            result = int.Parse(new string(simbols, 0,len));
+                            t = false;
+                            len = 0;
+                            continue;
+                        }
+
+                        if (len > 0)
+                        {
+                            numb = int.Parse(new string(simbols, 0,len));
+                            len = 0;
+                            if (diffs.Contains(numb))
+                            {
+                                End(1);
+                                return;
+                            }
+
+                            dif = result - numb;
+                            if (dif > 0)
+                                diffs.Add(dif);
+                        }
+
+                        continue;
+                    }
+
+                    len++;
                 }
 
-                if (end <= start)
-                    break;
+                if (len > 0)
+                {
+                    numb = int.Parse(new string(simbols, 0,len));
+                    if (diffs.Contains(numb))
+                    {
+                        End(1);
+                        return;
+                    }
+
+                    dif = result - numb;
+                    if (dif > 0)
+                        diffs.Add(dif);
+                }
             }
 
             End(0);
@@ -52,55 +73,6 @@ namespace Task3_Sum
         private static void End(int answer)
         {
             File.WriteAllText("output.txt", answer.ToString());
-        }
-
-        private static Tuple<int, int> TakeTarget()
-        {
-            var f = File.OpenRead("input.txt");
-            var buf = new byte[1];
-            var position = 0;
-            byte[] data;
-            while (true)
-            {
-                f.Read(buf, 0, 1);
-                if (buf[0] == 13)
-                {
-                    data = new byte[position];
-                    f.Position = 0;
-                    f.Read(data, 0, position);
-                    position += 2;
-                    return new Tuple<int, int>(int.Parse(Encoding.UTF8.GetString(data)), position);
-                }
-
-                position++;
-            }
-        }
-
-        private static List<int> ReadFile1()
-        {
-            var a = File.OpenRead("input.txt");
-            var answer = new List<int>();
-            var str = new StringBuilder(10);
-            var buf = new byte[1];
-            string ch;
-            while (a.Position<a.Length)
-            {
-                while (a.Position<a.Length)
-                {
-                    a.Read(buf, 0, 1);
-                    ch = Encoding.UTF8.GetString(buf);
-                    if (ch == " " || ch == "\n" || ch == "\r")
-                        break;
-
-                    str.Append(ch);
-                }
-
-                if(str.Length>0)
-                    answer.Add(int.Parse(str.ToString()));
-                str.Clear();
-            }
-
-            return answer;
         }
     }
 }
